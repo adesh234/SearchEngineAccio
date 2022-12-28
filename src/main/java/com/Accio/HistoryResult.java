d28212ba27ece20b39e969cd3eb4dc49bf62fcb3
Adesh <adeshsamdadiya@gmail.com>
2022-12-28 16:28:06 +0530

    Initial Commit

---
+++ src/main/java/com/Accio/HistoryResult.java
@@ -0,0 +1,23 @@
+package com.Accio;
+
+public class HistoryResult {
+    String keyword;
+    String Link;
+    public String getLink() {
+        return Link;
+    }
+
+    public void setLink(String link) {
+        Link = link;
+    }
+
+
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+}
